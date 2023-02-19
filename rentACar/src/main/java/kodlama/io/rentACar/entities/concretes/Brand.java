9ddc339536fcd271f78713117355d27193ab856e
Oğuzhan <oguzhnpala@gmail.com>
2023-02-19 23:54:28 +0300

    updated.

--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,13 +1,10 @@
 package kodlama.io.rentACar.entities.concretes;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,15 +20,13 @@ import lombok.Setter;
 @Entity
 public class Brand {
 	
-	@Id // pk 
+	@Id // primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
-	private int id;  //primary key pk
+	private int id;  
 	
 	@Column(name="name")
 	private String name;
 	
-	@OneToMany(mappedBy = "brand")
-	private List<Category> categories;
 
 }
